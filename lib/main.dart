30e80c750a06ec005162d54d02e1b182eaa3b411
suhaarshad <suhasyedarshad@gmail.com>
2024-04-03 00:09:21 +0500

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,91 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color(0xFFDBAFA0),
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
             children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images.jpg'),
+                radius: 60,
+              ),
+              Text('Suha Arshad',
+              style: TextStyle(
+                fontFamily: 'Merienda',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              color: Color(0xFF704264),
+              ),),
+
+              Text('FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'SourceCodePro',
+                fontSize: 18.0,
+                color: Color(0xFF704264),
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),),
+              SizedBox(height: 15,),
 
               Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.brown,
+                color:Color(0xFFc99a8b),
+                margin: EdgeInsets.symmetric(vertical:10,horizontal: 25 ),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.call),
+                    SizedBox(width: 10,),
+                    Text('+92 3343499967',
+                    style: TextStyle(
+                      fontFamily: 'SourceCodePro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0
+                    ),),
+                  ],
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
+
               Container(
-                height:double.infinity,
-                width: 100,
-                color: Colors.orange,
+                color:Color(0xFFc99a8b),
+                margin: EdgeInsets.symmetric(vertical:10,horizontal: 25 ),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.email),
+                    SizedBox(width: 10,),
+                    Text('suhaarshad37@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceCodePro',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),),
+                  ],
+                ),
+              ),
+
+              Container(
+                color:Color(0xFFc99a8b),
+                margin: EdgeInsets.symmetric(vertical:10,horizontal: 20 ),
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Icon(Icons.link),
+                    SizedBox(width: 10,),
+                    Text('https://github.com/suhaarshad',
+                      style: TextStyle(
+                          fontFamily: 'SourceCodePro',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0
+                      ),),
+                  ],
+                ),
               ),
 
 
-            ]
+            ],
           ),
         ),
       ),
